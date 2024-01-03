2ec473c5d925388b514c34c8b22ded1065eac4c8
uo293758 <uo293758@uniovi.es>
2024-01-03 15:24:44 +0100

    Añade diminutivos a los Encantamientos
    Se añade diminutivos a los encantamientos, que son un tipo enum, con
    estos se prevee poderse realizar las acciones de filtrado e información
    de los hoteles en el catálogo

--- src/uo/cpm/l7/model/Hotel/Encantamiento.java
+++ src/uo/cpm/l7/model/Hotel/Encantamiento.java
@@ -1,7 +1,20 @@
 package uo.cpm.l7.model.Hotel;
 
 public enum Encantamiento {
-	APARICIONES, DESCENSOS_TEMPERATURA, ENCENDIDO_APAGADO_LUCES, 
-	OBJETOS_CAMBIAN_LUGAR, OLORES_NAUSEABUNDOS, RUIDOS_EXTRANOS;
 	
+	APARICIONES("Ap"), DESCENSOS_TEMPERATURA("De"), ENCENDIDO_APAGADO_LUCES("En"), 
+	OBJETOS_CAMBIAN_LUGAR("Ob"), OLORES_NAUSEABUNDOS("Ol"), RUIDOS_EXTRANOS("Ru");
+	
+	public final String diminutivo;
+	
+	private Encantamiento(String diminutivo) {
+		this.diminutivo = diminutivo;
+	}
+	
+	/**
+	 * @return un String con el nombre de la imagen que representa al personaje
+	 */
+	public String getDiminutivo() {
+		return this.diminutivo;
+	}
 }
