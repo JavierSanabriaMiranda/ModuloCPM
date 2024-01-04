a0904314f9a85a002733b00b7cec451099ae3162
uo293758 <uo293758@uniovi.es>
2024-01-05 00:03:10 +0100

    Clase Encantamiento pasa a tener Atributo Nombre
    Se añade un nuevo atributo a los enums Encantamiento que representa el
    nombre completo del encantamiento

--- src/uo/cpm/l7/model/Hotel/Encantamiento.java
+++ src/uo/cpm/l7/model/Hotel/Encantamiento.java
@@ -2,19 +2,30 @@ package uo.cpm.l7.model.Hotel;
 
 public enum Encantamiento {
 	
-	APARICIONES("Ap"), DESCENSOS_TEMPERATURA("De"), ENCENDIDO_APAGADO_LUCES("En"), 
-	OBJETOS_CAMBIAN_LUGAR("Ob"), OLORES_NAUSEABUNDOS("Ol"), RUIDOS_EXTRANOS("Ru");
+	APARICIONES("Apariciones de fantasmas", "Ap"), DESCENSOS_TEMPERATURA("Descenso de temperatura", "De"), 
+	ENCENDIDO_APAGADO_LUCES("Encendido y apagado de luces", "En"), 
+	OBJETOS_CAMBIAN_LUGAR("Objetos que cambian de lugar" ,"Ob"), OLORES_NAUSEABUNDOS("Olores nauseabundos", "Ol"), 
+	RUIDOS_EXTRANOS("Ruidos extraños", "Ru");
 	
+	public final String nombreCompleto;
 	public final String diminutivo;
 	
-	private Encantamiento(String diminutivo) {
+	private Encantamiento(String nombreCompleto ,String diminutivo) {
+		this.nombreCompleto = nombreCompleto;
 		this.diminutivo = diminutivo;
 	}
 	
 	/**
-	 * @return un String con el nombre de la imagen que representa al personaje
+	 * @return un String con el diminutivo del encantamiento
 	 */
 	public String getDiminutivo() {
 		return this.diminutivo;
+	}
+	
+	/**
+	 * @return un String con el nombre completo del encantamiento
+	 */
+	public String getNombre() {
+		return this.nombreCompleto;
 	}
 }
